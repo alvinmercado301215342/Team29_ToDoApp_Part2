9d67db469eee13cafca0562c8242dfa893421fa1
Alvin Mercado <amercad8@my.centennialcollege.ca>
2021-11-28 08:47:58 -0500

    Initial Commit

---
+++ Team29_ToDoApp_Part1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Team29_ToDoApp_Part1
+//
+//  Created by Alvin Carl Mercado on 2021-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team29_ToDoApp_Part1/Team29_ToDoApp_Part1App.swift
@@ -0,0 +1,17 @@
+//
+//  Team29_ToDoApp_Part1App.swift
+//  Team29_ToDoApp_Part1
+//
+//  Created by Alvin Carl Mercado on 2021-11-28.
+//
+
+import SwiftUI
+
+@main
+struct Team29_ToDoApp_Part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
