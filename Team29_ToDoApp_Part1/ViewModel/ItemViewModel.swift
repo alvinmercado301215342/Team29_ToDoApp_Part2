4480cbb157c04674023a38bedc0541db89e31be1
Alvin Mercado <amercad8@my.centennialcollege.ca>
2021-11-28 19:48:44 -0500

    Added internal documents

--- Team29_ToDoApp_Part1/ViewModel/ItemViewModel.swift
+++ Team29_ToDoApp_Part1/ViewModel/ItemViewModel.swift
@@ -1,9 +1,11 @@
-//
-//  ItemViewModel.swift
-//  Team29_ToDoApp
-//
-//  Created by Alvin Carl Mercado on 2021-11-27.
-//
+// ToDo App Part 2
+// Name 01               : Alvin Carl Mercado
+// Student ID            : 301215342
+// Name 02               : Ishtiaque Ahmed
+// Student ID            : 301223010
+// Date last modified    : Nov. 28, 2021
+// Short revision history: App UI was initially created with one screen (7:00 PM 11/28/21) Created functionalities like add, edit and delete.
+// Short description of the program: This is a standard ToDo List app that is going to store to do list item inserted by the user.
 
 import SwiftUI
 import RealmSwift
